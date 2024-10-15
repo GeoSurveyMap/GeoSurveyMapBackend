34522dadda642281dbdc8366a4f34ad9faa8f5e3
Jan.Kaźmierczak <jkazmierczak.it@gmail.com>
2024-10-15 21:49:48 +0200

    [GEOSURVEYMAP-BACKEND] - operation description for registering user

--- src/main/kotlin/com/loess/geosurveymap/user/UserController.kt
+++ src/main/kotlin/com/loess/geosurveymap/user/UserController.kt
@@ -1,6 +1,7 @@
 package com.loess.geosurveymap.user
 
 import com.loess.geosurveymap.apiutils.ApiRequestHandler
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,6 +14,7 @@ class UserController(
     private val apiRequestHandler: ApiRequestHandler
 ) {
 
+    @Operation(summary = "Register a new user")
     @PostMapping
     fun register(@RequestBody user: User) = apiRequestHandler.handle {
         userService.registerUser(user)
